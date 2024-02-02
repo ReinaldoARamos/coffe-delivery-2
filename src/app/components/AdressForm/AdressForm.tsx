b948c79396c687ddb064c68577626f08e8a06b2a
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-02-02 11:25:56 -0300

    terminando o furmulário do endereço

--- src/app/components/AdressForm/AdressForm.tsx
+++ src/app/components/AdressForm/AdressForm.tsx
@@ -10,7 +10,7 @@ export function AdressForm() {
       <div className={`${inter.className} pb-4 text-lg font-bold`}>
         Complete seu pedido
       </div>
-    
+
       <div className="bg-base-card p-10">
         <div className="flex gap-2 pb-8">
           <MapPin size={22} className="text-brandyellow-dark" />
@@ -23,7 +23,41 @@ export function AdressForm() {
             </span>
           </div>
         </div>
-        <div>Form</div>
+        <form className="flex flex-col space-y-4">
+          <input
+            placeholder="CEP"
+            className="w-[200px] rounded border border-transparent bg-base-button p-3 text-sm leading-[130%] text-base-label transition-colors hover:cursor-pointer focus:border-brandyellow-dark focus:outline-none"
+          />
+          <input
+            placeholder="Rua"
+            className="rounded border border-transparent bg-base-button p-3 text-sm leading-[130%] text-base-label transition-colors hover:cursor-pointer focus:border-brandyellow-dark focus:outline-none"
+          />
+
+          <div className="flex gap-3">
+            <input
+              placeholder="Número"
+              className="w-[200px] flex-none rounded border border-transparent bg-base-button p-3 text-sm leading-[130%] text-base-label transition-colors hover:cursor-pointer focus:border-brandyellow-dark focus:outline-none"
+            />
+            <input
+              placeholder="Complementop"
+              className="flex-grow rounded border border-transparent bg-base-button p-3 text-sm leading-[130%] text-base-label transition-colors hover:cursor-pointer focus:border-brandyellow-dark focus:outline-none"
+            />
+          </div>
+          <div className="flex gap-3">
+            <input
+              placeholder="CEP"
+              className="w-[200px] flex-none rounded border border-transparent bg-base-button p-3 text-sm leading-[130%] text-base-label transition-colors hover:cursor-pointer focus:border-brandyellow-dark focus:outline-none"
+            />
+            <input
+              placeholder="CEP"
+              className="flex-grow  rounded border border-transparent bg-base-button p-3 text-sm leading-[130%] text-base-label transition-colors hover:cursor-pointer focus:border-brandyellow-dark focus:outline-none"
+            />
+            <input
+              placeholder="UF"
+              className="flex-grow  overflow-hidden rounded border border-transparent bg-base-button p-3 text-sm leading-[130%] text-base-label transition-colors hover:cursor-pointer focus:border-brandyellow-dark focus:outline-none"
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
