a0fa9b5785128a73a6399de79a5832cff178e59a
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-02-05 14:26:35 -0300

    criando uma função simples pra aumentar e diminuir a quantidade de cafés

--- src/app/components/CoffesAvailable/CoffesAvailable.tsx
+++ src/app/components/CoffesAvailable/CoffesAvailable.tsx
@@ -1,14 +1,30 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
-
+"use client";
 import { Minus, Plus, ShoppingCart } from "@phosphor-icons/react/dist/ssr";
 
 import { Baloo_2 } from "next/font/google";
+import { useState } from "react";
 
 const inter = Baloo_2({
   subsets: ["latin"],
 });
 export function CoffesCard() {
+  const [coffeCount, setCoffeeCount] = useState<number>(0);
+
+  function IncreaseCounter() {
+    const coffeeCounter = coffeCount;
+    setCoffeeCount(coffeeCounter + 1);
+  }
+
+  function decreaseCounter() {
+    const coffeeCounter = coffeCount;
+    setCoffeeCount(coffeeCounter - 1);
+
+    if (coffeeCounter <= 0) {
+      setCoffeeCount(0);
+    }
+  }
   return (
     <div className="flex w-64 flex-col items-center justify-center rounded-bl-[36px] rounded-br-[6px] rounded-tl-[6px] rounded-tr-[36px] bg-base-card px-5 pb-6 ">
       <img
@@ -38,11 +54,19 @@ export function CoffesCard() {
         </div>
         <div className="flex gap-2">
           <div className="flex items-center space-x-2">
-            <Minus size={14} className="text-brand-300 hover:cursor-pointer" />
-            <span>1</span>
-            <Plus size={14} className="text-brand-300 hover:cursor-pointer"  />
+            <Minus
+              size={14}
+              className="text-brand-300 hover:cursor-pointer"
+              onClick={() => decreaseCounter()}
+            />
+            <span>{coffeCount}</span>
+            <Plus
+              size={14}
+              className="text-brand-300 hover:cursor-pointer"
+              onClick={() => IncreaseCounter()}
+            />
           </div>
-          <div className="rounded-[6px] bg-brand-600 p-2 hover:bg-brand-300 transition-colors hover:cursor-pointer">
+          <div className="rounded-[6px] bg-brand-600 p-2 transition-colors hover:cursor-pointer hover:bg-brand-300">
             <ShoppingCart weight="fill" className="text-base-background " />
           </div>
         </div>
